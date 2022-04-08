18c2c54369b11753a6b0ccc3c210a56636948173
Matt Layher <mdlayher@planetscale.com>
2022-04-08 13:22:01 -0400

    main: remove uses of global state
    Signed-off-by: Matt Layher <mdlayher@planetscale.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"os"
 
@@ -27,81 +28,88 @@ type Category struct {
 	Description string
 }
 
-var DB *gorm.DB
+func main() {
+	// Load environment variables from file.
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
+	}
 
-func init() {
-	// Load environment variables
-	godotenv.Load()
-
-	// Connect to PlanetScale database
+	// Connect to PlanetScale database using DSN environment variable.
 	db, err := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{
 		DisableForeignKeyConstraintWhenMigrating: true,
 	})
-
 	if err != nil {
-		panic("failed to connect database")
+		log.Fatalf("failed to connect to PlanetScale: %v", err)
 	}
 
-	DB = db
-}
-
-func main() {
+	// Create a handler whose methods will be used to serve HTTP routes.
+	h := &handler{db: db}
 
 	r := mux.NewRouter()
-	// Run migrations and load sample data
-	r.HandleFunc("/seed", seedDatabase).Methods("GET")
-	r.HandleFunc("/products", getProducts).Methods("GET")
-	r.HandleFunc("/products/{id}", getProduct).Methods("GET")
-	r.HandleFunc("/categories", getCategories).Methods("GET")
-	r.HandleFunc("/categories/{id}", getCategory).Methods("GET")
+	r.HandleFunc("/seed", h.seedDatabase).Methods(http.MethodGet)
+	r.HandleFunc("/products", h.getProducts).Methods(http.MethodGet)
+	r.HandleFunc("/products/{id}", h.getProduct).Methods(http.MethodGet)
+	r.HandleFunc("/categories", h.getCategories).Methods(http.MethodGet)
+	r.HandleFunc("/categories/{id}", h.getCategory).Methods(http.MethodGet)
 
-	http.ListenAndServe(":8080", r)
+	// Start an HTTP API server.
+	const addr = ":8080"
+	log.Printf("successfully connected to PlanetScale, starting HTTP server on %q", addr)
+	if err := http.ListenAndServe(addr, r); err != nil {
+		log.Fatalf("failed to serve HTTP: %v", err)
+	}
 }
 
-func seedDatabase(w http.ResponseWriter, r *http.Request) {
-	DB.AutoMigrate(&Product{})
-	DB.AutoMigrate(&Category{})
+type handler struct {
+	db *gorm.DB
+}
 
-	DB.Create(&Category{Name: "Phone", Description: "Description 1"})
-	DB.Create(&Category{Name: "Video Game Console", Description: "Description 2"})
+func (h *handler) seedDatabase(w http.ResponseWriter, r *http.Request) {
+	h.db.AutoMigrate(&Product{})
+	h.db.AutoMigrate(&Category{})
 
-	DB.Create(&Product{Name: "iPhone", Description: "Description 1", Image: "Image 1", Category: Category{Id: 1}})
-	DB.Create(&Product{Name: "Pixel Pro", Description: "Description 2", Image: "Image 2", Category: Category{Id: 1}})
-	DB.Create(&Product{Name: "Playstation", Description: "Description 3", Image: "Image 3", Category: Category{Id: 2}})
-	DB.Create(&Product{Name: "Xbox", Description: "Description 4", Image: "Image 4", Category: Category{Id: 2}})
-	DB.Create(&Product{Name: "Galaxy S", Description: "Description 5", Image: "Image 5", Category: Category{Id: 1}})
+	h.db.Create(&Category{Name: "Phone", Description: "Description 1"})
+	h.db.Create(&Category{Name: "Video Game Console", Description: "Description 2"})
+
+	h.db.Create(&Product{Name: "iPhone", Description: "Description 1", Image: "Image 1", Category: Category{Id: 1}})
+	h.db.Create(&Product{Name: "Pixel Pro", Description: "Description 2", Image: "Image 2", Category: Category{Id: 1}})
+	h.db.Create(&Product{Name: "Playstation", Description: "Description 3", Image: "Image 3", Category: Category{Id: 2}})
+	h.db.Create(&Product{Name: "Xbox", Description: "Description 4", Image: "Image 4", Category: Category{Id: 2}})
+	h.db.Create(&Product{Name: "Galaxy S", Description: "Description 5", Image: "Image 5", Category: Category{Id: 1}})
 
 	w.Write([]byte("Migrations and Seeding of database complete"))
 }
 
-func getProducts(w http.ResponseWriter, r *http.Request) {
+func (h *handler) getProducts(w http.ResponseWriter, r *http.Request) {
 	products := []Product{}
-	DB.Preload("Category").Find(&products)
+	h.db.Preload("Category").Find(&products)
 	json.NewEncoder(w).Encode(products)
 }
-func getProduct(w http.ResponseWriter, r *http.Request) {
+
+func (h *handler) getProduct(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	product := Product{}
-	result := DB.First(&product, id)
+	result := h.db.First(&product, id)
 
 	if result.Error != nil {
 		w.Write([]byte("Product not found"))
 	} else {
 		json.NewEncoder(w).Encode(product)
 	}
-
 }
-func getCategories(w http.ResponseWriter, r *http.Request) {
+
+func (h *handler) getCategories(w http.ResponseWriter, r *http.Request) {
 	categories := []Category{}
-	DB.Find(&categories)
+	h.db.Find(&categories)
 	json.NewEncoder(w).Encode(categories)
 }
-func getCategory(w http.ResponseWriter, r *http.Request) {
+
+func (h *handler) getCategory(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	category := Category{}
-	result := DB.First(&category, id)
+	result := h.db.First(&category, id)
 
 	if result.Error != nil {
 		w.Write([]byte("Category not found"))
